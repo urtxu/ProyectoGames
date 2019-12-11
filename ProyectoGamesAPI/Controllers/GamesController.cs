a57cc8602ed74110c28b9cdf9a84361c9d678cc9
urtxu <gorkaurtxuegia97@gmail.com>
2019-12-11 09:28:21 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoGamesAPI/Controllers/GamesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProyectoGamesAPI.Models;
+using System.Web.Http.Cors;
+
+namespace ProyectoGamesAPI.Controllers
+{
+    public class GamesController : ApiController
+    {
+        private GamesModel db = new GamesModel();
+
+        // GET: api/Games
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IQueryable<Game> GetGames()
+        {
+            return db.Games;
+        }
+        // GET: api/Games/5
+        [ResponseType(typeof(Game))]
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult GetGame(int id)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+
+        // PUT: api/Games/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutGame(int id, Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != game.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(game).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Games
+        [ResponseType(typeof(Game))]
+        public IHttpActionResult PostGame(Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Games.Add(game);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (GameExists(game.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = game.ID }, game);
+        }
+
+        // DELETE: api/Games/5
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [ResponseType(typeof(Game))]
+        public IHttpActionResult DeleteGame(int id)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            db.Games.Remove(game);
+            db.SaveChanges();
+
+            return Ok(game);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GameExists(int id)
+        {
+            return db.Games.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ ProyectoGamesAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoGamesAPI.WebApiApplication" Language="C#" %>
